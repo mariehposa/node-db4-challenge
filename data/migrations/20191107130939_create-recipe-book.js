2034ba65d236671a5a0eae373334ea5eace42dc7
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-07 13:45:17 +0000

    create tables for imgredients and quantity

--- data/migrations/20191107130939_create-recipe-book.js
+++ data/migrations/20191107130939_create-recipe-book.js
@@ -5,6 +5,23 @@ exports.up = function(knex) {
         table.increments()
         table.string('recipe_name', 128)
     })
+    .createTable('ingredients', table => {
+        table.increments()
+        table.string('Ingredient_name', 128)
+    })
+    .createTable('quantity', table => {
+        table.increments()
+        table.integer('recipe_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .table('recipes')
+        table.integer('ingredient_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .table('ingerdients')
+    })
 };
 
 exports.down = function(knex) {
