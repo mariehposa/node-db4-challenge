3d5fb170ab55b5858dea6dfc4dc68a4fb9e443c2
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-07 19:10:31 +0000

    create models for recipe

--- data/migrations/20191107150320_create-recipe-book.js
+++ data/migrations/20191107150320_create-recipe-book.js
@@ -3,11 +3,11 @@
 exports.up = function (knex) {
     return knex.schema
         .createTable('recipes', table => {
-            table.increments('r_id')
+            table.increments('id')
             table.string('recipe_name', 128)
         })
         .createTable('ingredients', table => {
-            table.increments('i_id')
+            table.increments('id')
             table.string('Ingredient_name', 128)
         })
         .createTable('quantity', table => {
@@ -15,12 +15,12 @@ exports.up = function (knex) {
             table.integer('recipe_id')
                 .unsigned()
                 .notNullable()
-                .references('r_id')
+                .references('id')
                 .inTable('recipes')
             table.integer('ingredient_id')
                 .unsigned()
                 .notNullable()
-                .references('i_id')
+                .references('id')
                 .inTable('ingredients')
             table.decimal('quantity')
                 .unsigned()
@@ -31,7 +31,7 @@ exports.up = function (knex) {
             table.integer('recipe_id')
                 .unsigned()
                 .notNullable()
-                .references('r_id')
+                .references('id')
                 .inTable('recipes')
             table.integer('step_number')
                 .unsigned()
---
+++ recipes/recipeModel.js
@@ -0,0 +1,24 @@
+const db = require('../data/dbConfig');
+
+function getRecipes () {
+    return db('recipes')
+}
+
+function getShoppingList(recipe_id) {
+    return db('quantity as q')
+    .join('ingredients as i', 'q.ingredient_id', 'i.id')
+    .select('i.Ingredient_name', 'q.quantity')
+    .where('q.recipe_id', recipe_id)
+}
+
+function getInstructions(recipe_id) {
+    return db('steps as s')
+    .select('s.step_number', 's.Instruction')
+    .where('s.recipe_id', recipe_id)
+}
+
+module.exports = {
+    getRecipes,
+    getShoppingList,
+    getInstructions,
+}
