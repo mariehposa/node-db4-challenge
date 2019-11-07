534ce3e9c9ec0346ac8458b39e3f3c75602249af
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-07 13:00:49 +0000

    configure knexfile

---
+++ knexfile.js
@@ -0,0 +1,20 @@
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './data/recipeBook.db3'
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
+    pool: {
+      afterCreate: (connection, done) => {
+        connection.run('PRAGMA foreign keys = ON', done);
+      }
+    }
+  },
+};
